b5413057cee3862e89aac166fed4d464961cc527
lzm2013lzm <lzm2013lzm@gmail.com>
2021-01-07 11:21:21 +0800

    The Asset Repository

--- app/code/Codilar/Demo/Block/Head.php
+++ app/code/Codilar/Demo/Block/Head.php
@@ -2,4 +2,14 @@
 namespace Codilar\Demo\Block;
 class Head extends \Magento\Framework\View\Element\Template
 {
+    public $assetRepository;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = [],
+        \Magento\Framework\View\Asset\Repository $assetRepository
+    )
+    {
+        $this->assetRepository = $assetRepository;
+        return parent::__construct($context, $data);
+    }
 }
