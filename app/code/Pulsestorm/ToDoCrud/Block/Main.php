a9bb32a1000757df0ad572f0c938a0a48fb82a05
lzm2013lzm <lzm2013lzm@gmail.com>
2021-01-08 14:07:25 +0800

    saved the information

--- app/code/Pulsestorm/ToDoCrud/Block/Main.php
+++ app/code/Pulsestorm/ToDoCrud/Block/Main.php
@@ -6,8 +6,23 @@ namespace Pulsestorm\ToDoCrud\Block;
  */
 class Main extends \Magento\Framework\View\Element\Template
 {
+    protected $toDoFactory;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Pulsestorm\ToDoCrud\Model\TodoItemFactory $toDoFactory
+    )
+    {
+        $this->toDoFactory = $toDoFactory;
+        parent::__construct($context);
+    }
     function _prepareLayout(){
-        var_dump("I am Here");
+        $todo = $this->toDoFactory->create();
+        $todo->setData('item_text','Finish my Magento article')
+            ->save();
+        var_dump('Done');
         exit;
+//        $model = Mage::getModel('pulsestorm_todocrud/todoitem')
+//            ->setItemText('Finish my Magento Article')
+//            ->save();
     }
 }
