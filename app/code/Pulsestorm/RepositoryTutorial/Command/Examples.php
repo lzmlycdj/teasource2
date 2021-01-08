7f8e3fcaee16205a375224b3ffa36fa6f887a602
lzm2013lzm <lzm2013lzm@gmail.com>
2021-01-08 18:11:19 +0800

    retrieve cms page title (id=2) using CMS page object

--- app/code/Pulsestorm/RepositoryTutorial/Command/Examples.php
+++ app/code/Pulsestorm/RepositoryTutorial/Command/Examples.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Examples extends Command
 {
+    protected $objectManager;
     protected function configure()
     {
         $this->setName("ps:examples");
@@ -14,8 +15,27 @@ class Examples extends Command
         parent::configure();
     }
 
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\App\State $appState,
+        $name=null
+    )
+    {
+        $this->objectManager = $objectManager;
+        $appState->setAreaCode('frontend');
+        parent::__construct($name);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Hello World");
+         $repo = $this->objectManager->get('Magento\Cms\Model\PageRepository');
+         $page = $repo->getById(2);
+         echo $page->getTitle(),"\n";
+        exit;
+        echo get_class($page),"\n";
+        /*$repo = $this->objectManager->get('Magento\Catalog\Model\ProductRepository');
+        $repo->getList();*/
     }
+
+
 } 
