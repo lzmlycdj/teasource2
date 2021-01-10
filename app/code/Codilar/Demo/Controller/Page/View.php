64c7567a68f40aed265287b34845956ba036ea02
lzm2013lzm <lzm2013lzm@gmail.com>
2021-01-10 14:50:41 +0800

    create my first page through layout

---
+++ app/code/Codilar/Demo/Controller/Page/View.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+namespace Codilar\Demo\Controller\Page;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Result\Page;
+
+class View extends Action
+{
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
